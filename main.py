c8143b8cd48b7ae71ec0fdcdeb970ba029c4c613
D3lK1ch1 <dhizkch@gmail.com>
2026-05-14 09:25:18 +1000

    Set items' area on WP

--- main.py
+++ main.py
@@ -1662,6 +1662,7 @@ def create_devops_ticket():
         {'op': 'add', 'path': '/fields/Microsoft.VSTS.Common.Priority',           'value': az_priority},
         {'op': 'add', 'path': '/fields/System.AssignedTo',                        'value': sm_email},
         {'op': 'add', 'path': '/fields/Microsoft.VSTS.Common.AcceptanceCriteria', 'value': ac_html},
+        {'op': 'add', 'path': '/fields/System.AreaPath',                            'value': f'{project}\\WP'},
         {'op': 'add', 'path': '/relations/-', 'value': {
             'rel': 'System.LinkTypes.Hierarchy-Reverse',
             'url': f'https://dev.azure.com/{org}/_apis/wit/workitems/{parent_id}',
